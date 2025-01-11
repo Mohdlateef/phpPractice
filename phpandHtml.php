85365987e909682f6b491e9b913a0f65a823c152
mohdlateef <lateefahbaba123@gmail.com>
2025-01-11 22:36:33 +0530

    using html and css in php and vice versa

---
+++ phpandHtml.php
@@ -0,0 +1,23 @@
+<?php
+echo "<h1 style='color:yellow'> hey my name is lateef ahmad baba</h1>"
+
+
+?>
+<?php
+$text="using php in html";
+$red="red";
+$green="green";
+$blue="blue";
+?>
+
+<h1>
+    <?php
+echo $text;
+    ?>
+</h1>
+
+<h1 style='color:<?php echo $red;?>'>
+    <?php
+echo $text;
+    ?>
+</h1>
