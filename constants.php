36f52f0f10fd3c221ddee3974fc6055adcb16fe3
mohdlateef <lateefahbaba123@gmail.com>
2025-01-12 11:11:15 +0530

    constants completed

---
+++ constants.php
@@ -0,0 +1,19 @@
+<?php
+const name="umer";
+
+echo(name);
+
+name="lateef";
+echo name;
+?>
+
+
+<?php
+$name="umer";
+echo $name,"</br>";
+$name="lateef";
+
+echo $name;
+
+
+?>
--- phpandHtml.php
+++ phpandHtml.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1 style='color:yellow'> hey my name is lateef ahmad baba</h1>"
+echo "<h1 style='color:yellow'> hey my name is lateef ahmad baba</h1>";
 
 
 ?>
