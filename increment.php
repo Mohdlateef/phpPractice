1f97e3b3f8175c41999fb7bfaf852dd2551f7afe
mohdlateef <lateefahbaba123@gmail.com>
2025-01-14 22:15:07 +0530

    string oprators completed

--- increment.php
+++ increment.php
@@ -1,6 +1,6 @@
 <?php
 $a=10;
-echo($a++); //post incriment 
+echo($a++); //post incriment 10+1
 echo "</br>";
 echo "</br>";
 echo "</br>";
---
+++ stringsandstringoprators.php
@@ -0,0 +1,14 @@
+<?php
+$str="umer";
+echo "my name is $str";//this will work;
+echo "</br>";
+echo 'my name is $str'.$str;//concaticate oprator
+echo "</br>";
+$str1="hey";
+$str2="how are you ";
+echo "<h1>before using concatination oprator :  $str1</h1>";
+$str1.=$str2;
+
+echo "<h1>after using concatination oprator :   $str1</h1>"
+
+?>
