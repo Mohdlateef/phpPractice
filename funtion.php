32feb2c6f2d58cc53e8beb33239b8a80b8a69f1d
mohdlateef <lateefahbaba123@gmail.com>
2025-01-16 22:26:36 +0530

    basic functions completed

--- funtion.php
+++ funtion.php
@@ -9,7 +9,24 @@ function fun1(){
  function fun2()
  {
     fun1();
-    echo "funtion inside function ";
+    echo "funtion inside function <br>";
  }
  fun2();
+
+ function fun3($name,$roll,$sec)
+ {
+    echo "$name,$roll,$sec <br>";
+ }
+
+ fun3("lateef",11,"a");
+ fun3("umer",12,"a");
+ 
+ function fun4()
+ {
+    return "hey are you there";
+ }
+ $fundata=fun4();
+echo "$fundata";
+
+
 ?>
