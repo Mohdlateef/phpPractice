086dbf608e519e38a66d167712707abd12423cba
mohdlateef <lateefahbaba123@gmail.com>
2025-01-16 22:19:10 +0530

    function call inside funtion

--- funtion.php
+++ funtion.php
@@ -1,6 +1,15 @@
 <?php
 function fun1(){
-    echo "<h1>hey from fun 1 </h1>";
+    // fun2(); this is not possible becoz it runs infinite times 
+
+    echo "<h1>hey from fun 1 </h1><br>";
 }  
  fun1();
+
+ function fun2()
+ {
+    fun1();
+    echo "funtion inside function ";
+ }
+ fun2();
 ?>
