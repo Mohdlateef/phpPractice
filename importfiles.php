de81c1a4ab2a8e6a6799a4e20f8bfc0ddd88c295
mohdlateef <lateefahbaba123@gmail.com>
2025-01-18 11:25:40 +0530

    file imports completd

---
+++ importfiles.php
@@ -0,0 +1,16 @@
+<?php
+// include("./test.php/"); 
+
+require("./test.php");
+
+
+for($i=0;$i<=10;$i++)
+{
+require_once("./test.php");
+echo"<br>";
+}
+
+
+
+
+?>
--- test.php
+++ test.php
@@ -1,3 +1,4 @@
 <?php
 echo 40/10;
+echo "<h1>hello from test.php</h1>"
 ?>
