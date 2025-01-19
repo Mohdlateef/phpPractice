7fc4d4e57dc7c31cc4b90a35ac5f89d3384b630e
mohdlateef <lateefahbaba123@gmail.com>
2025-01-19 15:38:34 +0530

    static binding completed

--- staticMethod.php
+++ staticMethod.php
@@ -9,4 +9,25 @@ class Student1{
 Student1::marks();
 echo"<br>";
 echo Student1::$name;
+
+
+//static binding
+
+class c1{
+
+    static function f1()
+    {
+    echo "from c1...";
+    }
+}
+
+class c2 extends c1{
+
+   static function f1()
+    {
+    echo "from c2";
+    }
+}
+c1::f1();
+c2::f1();
 ?>
